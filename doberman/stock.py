56e401a8e217d5b798f4fb005f850abff7e799de
Brian Goldberg <brianxgoldberg@gmail.com>
2024-01-27 11:32:15 -0500

    fix tests

--- doberman/stock.py
+++ doberman/stock.py
@@ -20,7 +20,7 @@ class Stock:
         self._ohlc_tsdb = OHLC(ticker, date_start, date_end, self.config)
 
     def load_data(self):
-
+        """ Read data from HDF5 file and load into Pandas dataframe """
         self._ohlc_tsdb.load_data()
         self._ohlc_tsdb.snip_dates()
         self._ohlc_tsdb.calc_pct_ret()
@@ -28,44 +28,53 @@ class Stock:
         self._trade_log = TradeLog(self.ticker, self.config, 
                     self._ohlc_tsdb.ohlc.index[0])
         
-    def log_trade(self, trade_date, order_type, shares, trade_cost):
-
+    def log_trade(self, trade_date:str , order_type: str, shares: int, trade_cost: float):
+        # Interface to TradeLog.log_trade method
         if trade_date not in self._ohlc_tsdb.ohlc.index:
             raise Exception(f'{trade_date} not in time series')
 
         self._trade_log.log_trade(trade_date, order_type, shares, trade_cost)
-        logger.debug(f'Trade logged: {trade_date.strftime("%Y-%m-%d")} {self.ticker.upper()} {order_type} {shares} @ ${round(trade_cost, 2)}')
 
-    def usd_position(self, trade_date=None):
-        return self._trade_log.usd_position(trade_date)
-
-    def shares_held(self, trade_date=None):
+    def shares_held(self, trade_date=None) -> float:
+        # Interface to TradeLog.shares_held method
         return self._trade_log.shares_held(trade_date)
 
-    def position_value(self, trade_date=None):
-        pv = self.shares_held(trade_date)  * self.spot_price(trade_date) \
-                + self.usd_position(trade_date) 
+    def cash_position(self, trade_date=None):
+        # Interface to TradeLog.cash_position method
+        return self._trade_log.cash_position(trade_date)
+
+    def position_value(self, trade_date=None) -> float:
+        """ Value of shares held at specific date plus amount of cash on hand """
+        pv = (self.shares_held(trade_date)  * self.spot_price(trade_date)) \
+                + self.cash_position(trade_date) 
         return pv
 
     def max_drawdown(self, trade_date=None):
         return self._trade_log.max_drawdown(trade_date)
 
-    def spot_price(self, trade_date=None, col='close'):
+    def spot_price(self, trade_date=None, col='close') -> float:
+        """ 
+        Return stock price given a specific date. Use selected column (or close)
+        for query. Will return last price from OHLC dataset if date not given.
+        """
         if not trade_date:
-            trade_date = self.trade_log.index[-1]
+            trade_date = self._ohlc_tsdb.ohlc.index[-1]
 
         if trade_date not in self._ohlc_tsdb.ohlc.index:
             #raise Exception(f'{trade_date} not in time series')
             logger.error(f'{self.ticker.upper()} {trade_date} not in time series')
 
-        return self.ohlc.loc[trade_date][col]
+        return self._ohlc_tsdb.ohlc.loc[trade_date][col]
 
-    def rate_of_return(self, trade_date=None):
+    def rate_of_return(self, trade_date=None) -> float:
+        """
+        Calc RoR - Present value (shares + cash) / max-drawdown
+        """
         if not trade_date:
             trade_date = self.ohlc.index[-1]
         
         share_value = self.shares_held(trade_date) * self.spot_price(trade_date) 
-        present_value = share_value + self.usd_position(trade_date)
+        present_value = share_value + self.cash_position(trade_date)
         ror = present_value / abs(self.max_drawdown(trade_date))
 
         return ror
@@ -109,7 +118,13 @@ class OHLC:
         logger.info(f'Calculated pct return for {self.ticker.upper()}')
 
 class TradeLog:
+    """
+    TradeLog is a dataframe which holds records of tranactions. Methods calculate
+    the sum cost of all transactions and the maximum amount drawn down from stock
+    purchases.
 
+    Timeseries is limited to the index of logged trades.
+    """
     def __init__(self, ticker, config, tl_index):
 
         self.ticker = ticker
@@ -120,8 +135,10 @@ class TradeLog:
         self.trade_log['order_type'] = None
         self.trade_log['trade_cost'] = 0        # total cost of trade
 
-    def log_trade(self, trade_date, order_type, shares, trade_cost):
-
+    def log_trade(self, trade_date:str , order_type:str, shares:int, trade_cost:float):
+        """
+        Input trade date, type, qty and total cost of tranaction to trade log.
+        """
         trade_cost = round((trade_cost), 2)
 
         if order_type == 'buy':
@@ -130,23 +147,33 @@ class TradeLog:
             shares = shares * -1
 
         self.trade_log.loc[trade_date, ['shares', 'order_type', 'trade_cost']] = [shares, order_type, trade_cost]
-        logger.debug(f'Trade logged: {trade_date.strftime("%Y-%m-%d")} {self.ticker.upper()} {order_type} {shares} @ ${round(trade_cost, 2)}')
+        logger.debug(f'Trade logged: {trade_date} {self.ticker.upper()} {order_type} {abs(shares)} @ ${round(trade_cost, 2)}')
 
-    def usd_position(self, trade_date=None):
-
-        if not trade_date:
-            trade_date = self.trade_log.index[-1]
-        usd = self.trade_log['trade_cost'].loc[:trade_date].sum()
-        return round(usd, 2)
-
-    def shares_held(self, trade_date=None):
-
+    def shares_held(self, trade_date=None) -> float:
+        """
+        Return the sum of all shares held from beginning of the tradelog index up until
+        a specified date (or last trade if date not provided)
+        """
         if not trade_date:
             trade_date = self.trade_log.index[-1]
         return self.trade_log['shares'].loc[:trade_date].sum()
 
-    def max_drawdown(self, trade_date=None):
+    def cash_position(self, trade_date=None) -> float:
+        """
+        Return the sum of all trade costs from beginning of the tradelog index up until
+        a specified time (or the whole series if not specified). Can be positive or negative
+        based upon how much you've bought or sold.
+        """
+        if not trade_date:
+            trade_date = self.trade_log.index[-1]
+        cash = self.trade_log['trade_cost'].loc[:trade_date].sum()
+        return round(cash, 2)
 
+    def max_drawdown(self, trade_date=None) -> float:
+        """
+        Return the maximum USD amount drawn down in the time series. This is the 
+        maximum number in the time series, not a summation of the current amount.
+        """
         if not trade_date:
             trade_date = self.trade_log.index[-1]
         return self.trade_log['trade_cost'].loc[:trade_date].cumsum().min()
--- tests/test_stock.py
+++ tests/test_stock.py
@@ -8,57 +8,65 @@ app_path = os.path.join(os.path.expanduser('~/sandbox/doberman'))
 sys.path.append(app_path)
 from doberman import Stock
 
-symbol = 'spy'
-date_start = '2015-01-01'
-date_end = '2015-01-28'
+symbol = 'brk-a'
+date_start = '2015-06-01'
+date_end = '2015-06-30'
 trades = {
-    '2015-01-02': ('buy', 50),
-    '2015-01-07': ('sell', 50),
-    '2015-01-12': ('buy', 56),
-    '2015-01-21': ('sell', 50),
-    '2015-01-27': ('buy', 60) }
-"""
-DATE        PRICE
-2015-01-02  179.005
-2015-01-07  176.267
-2015-01-12  176.566
-2015-01-21  176.984
-2015-01-27  176.655
-"""
+    '2015-06-02': ('buy', 1),
+    '2015-06-05': ('sell', 1),
+    '2015-06-12': ('buy', 1),
+    '2015-06-22': ('sell', 1),
+    '2015-06-29': ('buy', 1) 
+    }
 
-stock = Stock(symbol, date_start, date_end, config='../config.toml')
+CONFIG = os.path.expanduser('~/sandbox/doberman/config.toml')
+stock = Stock(symbol, date_start, date_end, config=CONFIG)
 stock.load_data()
 
 def test_configuration():
+    # TEST CONFIGURATION
     # Read configuration and check a parameter settting.
+    
     cfg_val = stock.config['data_map'].get('spot_quote_col', '')
     assert cfg_val == 'close'
 
-def test_data_load(test_date='2015-01-09'):
+def test_load_data(test_date='2015-06-09'):
+    # TEST OHLC CLASS - data loading and date snipping
     # Read dataframe and confirm trade date / price match.
-    #row_open = stock.ohlc.loc[test_date]['open']
-    #row_close = stock.ohlc.loc[test_date]['close']
+    
     row_open = stock.spot_price(test_date, 'open')
     row_close = stock.spot_price(test_date, 'close')
 
-    assert [row_open, row_close] == approx([179.82, 177.96], abs=1)
+    assert [row_open, row_close] == approx([209400.00, 209700.00], abs=1)
 
-def test_log_trades(test_date='2015-01-13'):
+def test_log_trades(test_date='2015-06-15'):
     # Add trades to trade log. Confirm shares held and USD position
     # at specified date.
+    
     col_name = stock.config['data_map'].get('spot_quote_col', 'close')
 
+    # Subimt fake orders
     for trade_date, stock_order in trades.items():
         order_type, shares = stock_order
         trade_cost = shares * stock.spot_price(trade_date, col_name)
         stock.log_trade(trade_date, order_type, shares, trade_cost)
 
     results = [
-        stock.shares_held(test_date), stock.usd_position(test_date),
-        stock.shares_held(), stock.usd_position()
-    ]
+        stock.trade_log.loc['2015-06-05'].shares,
+        stock.trade_log.loc['2015-06-05'].order_type,
+        stock.trade_log.loc['2015-06-05'].trade_cost ]
 
-    assert results == approx([56.0, -10024.6, 66, -11774.7], abs=1)
-    
-def test_pnl_report():
-    pass
+    assert results == [-1.0, 'sell', 211560.0]
+
+def test_trade_log_stats(test_date='2015-06-30'):
+    # Test calculation of cash position, shares held and max
+    # drawdown
+
+    results = [
+        stock.shares_held(test_date),
+        stock.cash_position(test_date),
+        stock.shares_held('2015-06-15'),
+        stock.cash_position('2015-06-15'),
+        stock.max_drawdown()]
+
+    assert results == [1, -206440, 1, -214020, -214820.0]
